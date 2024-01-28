46c2686c074082a5cfd647b9415156496b578069
RattanakpanhaKONG <krattanakpanha@gmail.com>
2024-01-28 23:15:49 +0700

    Install Flowbite and Navbar

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  email: string = '';
+  password: string = '';
+  errorMsg: string = '';
+
+  constructor(){
+
+  }
+  ngOnInit(): void {
+    
+  }
+
+  login(){
+    if(this.email.trim().length === 0){
+      this.errorMsg = 'Email is required'
+    }
+    else if(this.password.trim().length === 0){
+      this.errorMsg = 'Password is required'
+    }
+    else{
+      this.errorMsg = ''
+    }
+  }
+
 
 }
